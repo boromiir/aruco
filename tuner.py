4f018ada657458ce99aa6b999ea45323126f3817
pszemek111 <60076005+pszemek111@users.noreply.github.com>
2020-02-29 16:02:38 +0100

    Update tuner.py

--- tuner.py
+++ tuner.py
@@ -2,90 +2,33 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
 import glob
+from datetime import datetime
 
-marker_size = 0.12
 cap = cv2.VideoCapture(0)
+marker_length = 0.125
 
-def get_x_y_z(tvec, rotation, tvec_relative=[marker_size/2, marker_size/2, 0]):
-    
-    tvec_relative = np.expand_dims(np.array(tvec_relative), -1)
-    
-    print('tvec', tvec)
-    print('rot', np.dot(rotation,tvec_relative) )
-    x_y_z = tvec + np.squeeze(np.dot(rotation,tvec_relative))
-    print('x_y_z is', x_y_z)
-    return x_y_z
 
-def raytracing(x,y,z, focal):
-    assert z!=0
-    x_on_screen = x/z*focal
-    y_on_screen = y/z*focal
-    return x_on_screen, y_on_screen
+with open('test.ply', 'w') as file:
+    file.write('ply\n')
+    file.write('format ascii 1.0\n')
+    file.write('element vertex WPISZ\n')
+    file.write('property float x\nproperty float y\nproperty float z\nproperty uchar red\nproperty uchar green\nproperty uchar blue\nend_header\n')
 
-def drawer_point(frame, x, y):
-    y_max,x_max, *non_used = frame.shape
-    try:
-        assert 0 < x < x_max
-    except AssertionError as e:
-        print(e, x, x_max)
-    try:
-        assert 0 < y < y_max
-    except AssertionError as e:
-        print(e, y, y_max)
-    center_coordinates = (int(x), int(y)) 
-  
-    # Radius of circle 
-    radius = 10
-    
-    # Blue color in BGR 
-    color = (255, 0, 0) 
-    
-    # Line thickness of 2 px 
-    thickness = 7
-    
-    # Using cv2.circle() method 
-    # Draw a circle with blue line borders of thickness of 2 px 
-    frame_circled = cv2.circle(frame, center_coordinates, radius, color, thickness, lineType=-1) 
-    return frame_circled
+cam_pos=[]
 
-def drawer_rect(frame, x1, y1, x2, y2):
-    y_max,x_max, *non_used = frame.shape
-    try:
-        assert 0 < x < x_max
-    except AssertionError as e:
-        print(e, x, x_max)
-    try:
-        assert 0 < y < y_max
-    except AssertionError as e:
-        print(e, y, y_max)
-    pt1 = (int(x1), int(y1)) 
-    pt2 = (int(x2), int(y2))
-    
-    # Blue color in BGR 
-    color = (255, 0, 0) 
-    
-    # Line thickness of 2 px 
-    thickness = 2
-    
-    # Using cv2.circle() method 
-    # Draw a circle with blue line borders of thickness of 2 px 
-    frame_circled = cv2.rectangle(frame, pt1, pt2, color, thickness) 
-    return frame_circled
-
-focal = 650
+focal = 463.36172654
 width, height = 640,480
-mtx = np.array([[  focal,   0.00000000e+00,   width//2],
-       [  0.00000000e+00,   focal,   height//2],
-       [  0.00000000e+00,   0.00000000e+00,   1.00000000e+00]])
+mtx = np.array([[463.36172654, 0.00000000e+00, 321.97022644],
+                [0.00000000e+00, 464.1197524, 239.66525775],
+                [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
 
-dist = np.array([[-0.12481632, -0.00360626,  0.00294025, -0.00138677,  0.52909508]])
-
+dist = np.array([[ 1.43799762e-01, -4.31395968e-01, -2.65579956e-04, -4.44358664e-03,
+   3.26025213e-01]])
 
 ###------------------ ARUCO TRACKER ---------------------------
-while(1):
+detections = 0
+while (True):
     ret, frame = cap.read()
-    if frame is None: 
-        continue
     print(frame.shape)
     # operations on the frame
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -106,46 +49,68 @@ while(1):
     # check if the ids list is not empty
     # if no check is added the code will crash
     if np.all(ids != None):
-        print("znaleziono coś")
+
         # estimate pose of each marker and return the values
         # rvet and tvec-different from camera coefficients
-        rvec, tvec ,_ = aruco.estimatePoseSingleMarkers(corners, marker_size, mtx, dist)
-        #(rvec-tvec).any() # get rid of that nasty numpy value array error
-        rotation = cv2.Rodrigues(rvec)[0]
-        #print("rodriguez to", cv2.Rodrigues(rvec))
-        for i in range(0, ids.size):
-            # draw axis for the aruco markers
-            cv2.imwrite("good.png", frame)
-            aruco.drawAxis(frame, mtx, dist, rvec[i], tvec[i], 0.1)
-            cv2.imwrite("with_axis.png", frame)
-            #print(rotation)
-            x,y,z = get_x_y_z(tvec[0,0], rotation, [0.01*1.5*12, -0.01*7.3*12, 0.01*0.5*12])
-            x1,y1 = raytracing(x, y, z, focal)
-            x,y,z = get_x_y_z(tvec[0,0], rotation, [0.01*2.9*12, -0.01*9.0*12, 0.01*0.5*12])
-            x2,y2 = raytracing(x, y, z, focal)
-            frame = drawer_rect(frame, x1+width/2,y1+height/2, x2+width/2,y2+height/2)
-            
-            #frame = drawer_point(frame, x2+width/2,y2+height/2)
+        rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, marker_length, mtx, dist)
+        # (rvec-tvec).any() # get rid of that nasty numpy value array error
+        if rvec is None:
+            continue
+        for coor in range(rvec.shape[0]):
+
+            for i in range(0, ids.size):
+                # draw axis for the aruco markers
+                print(tvec[i])
+                cv2.putText(frame, "tvec" + "{0:.2f}".format(tvec[i][0][0]) + " " + "{0:.2f}".format(
+                    tvec[i][0][1]) + " " + "{0:.2f}".format(tvec[i][0][2]), (0, (i+1)*32), font, 1, (0, 255, 0), 2,
+                            cv2.LINE_AA)
+                cv2.putText(frame, "rvec" + "{0:.2f}".format(rvec[i][0][0]) + " " + "{0:.2f}".format(
+                    rvec[i][0][1]) + " " + "{0:.2f}".format(rvec[i][0][2]), (0, (i + 2) * 32), font, 1, (255, 255, 0), 2,
+                            cv2.LINE_AA)
+                # cv2.imwrite('raw/'+str(detections) + "good.png", frame)
+                aruco.drawAxis(frame, mtx, dist, rvec[i], tvec[i], 0.1)
+                # cv2.imwrite('axis/'+str(detections) + "with_axis.png", frame)
+                detections += 1
         # draw a square around the markers
         aruco.drawDetectedMarkers(frame, corners)
-
 
         # code to show ids of the marker found
         strg = ''
         for i in range(0, ids.size):
-            strg += str(ids[i][0])+', '
+            strg += str(ids[i][0]) + ', '
+            if ids[i][0] == 0:
+                cam_pos.append((-0.6-tvec[i][0][0] , 1.32-tvec[i][0][1] , 3-tvec[i][0][2] ))
+                print("ID 1 dodano")
+            if ids[i][0] == 1:
+                cam_pos.append((-0.6-tvec[i][0][0] , 1.32-tvec[i][0][1] , 3.6-tvec[i][0][2] ))
+                print("ID 2 dodano")
+            if ids[i][0] == 2:
+                cam_pos.append((-0.6-tvec[i][0][0], 1.32- tvec[i][0][1] , 4.2-tvec[i][0][2]))
+                print("ID 3 dodano")
 
-        cv2.putText(frame, "Id: " + strg, (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
+        cv2.putText(frame, "Id: " + strg, (0, 420), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
+        xp5pix=(tvec[0][0][0]+0.05)*focal/tvec[0][0][2]
+        xm5pix=(tvec[0][0][0]-0.05)/tvec[0][0][2]
+        y10pix=(tvec[0][0][1]+0.1)*focal/tvec[0][0][2]
 
+        cv2.line(frame,(int(xm5pix+width//2), int(y10pix+height//2)),(int(xp5pix+width//2), int(y10pix+height//2)),(255,0,255,3))
+        #cv2.circle(frame,(int(xpix+width//2), int(y10pix+height//2)), 5, (255, 0, 255), 2)
+        print(len(cam_pos))
+        f= open("test.ply","a")
+        with f:
+            if len(cam_pos):
+                asd="{} {} {} 0 255 255\n".format(cam_pos[len(cam_pos)-1][0],cam_pos[len(cam_pos)-1][1],cam_pos[len(cam_pos)-1][2])
+                print("zapisuje")
+                f.write(asd)
     else:
         # code to show 'No Ids' when no markers are found
-        cv2.putText(frame, "No Ids", (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
+        cv2.putText(frame, "No Ids", (0, 440), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
 
     # display the resulting frame
-
-    cv2.imshow('frame',frame)
-    cv2.waitKey(10)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # When everything done, release the capture
 cap.release()
